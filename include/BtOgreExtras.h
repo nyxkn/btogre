135af451d9fa928ca734437066373af464e954c4
Arthur Brainville <ybalrid@ybalrid.info>
2017-03-04 18:45:35 +0100

    Add a way to set the color multiplier for HDR

--- include/BtOgreExtras.h
+++ include/BtOgreExtras.h
@@ -193,6 +193,12 @@ public:
     ~DebugDrawer()
     {
     }
+    
+    void setUnlitDiffuseMultiplier(float value)
+    {
+    	if(value >= 1) unlitDiffuseMultiplier = value;
+    }
+    
 
     void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override
     {
